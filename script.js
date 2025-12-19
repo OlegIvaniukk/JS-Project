18a7b6e0b69f94fc01b4f025e79c95129cf184bb
Olegivaniukk <olegivaniuk@gmail.com>
2025-12-19 19:37:02 +0200

    2 com

--- script.js
+++ script.js
@@ -13,3 +13,37 @@ const filmRating = prompt('How do you rate it?', '');
 personalMovieDB.movies[lastFilm] = filmRating;
 
 console.log(personalMovieDB);
+
+let result = '';
+
+for (let i = 1; i < 7; i++) {
+  for (let j = 0; j < i; j++) {
+    result += '*';
+  }
+  result += '\n';
+}
+
+console.log(result);
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// Место для третьей задачи
+function thirdTask() {
+  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+  const data = [5, 10, 'Shopping', 20, 'Homework'];
+  const result = [];
+
+  // Пишем решение вот тут
+  for (let i = data.length - 1; i >= 0; i--) {
+    result.push(data[i]);
+  }
+  console.log(result);
+  // Не трогаем
+  return result;
+}
